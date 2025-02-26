00d83ea4779339ed778fb7d8a7588039f6908bc8
David Torres <davidj_torres@hotmail.com>
2025-02-26 10:45:13 -0500

    adding attributes, serializer and default eventType

--- src/events/base/HeliosEvent.ts
+++ src/events/base/HeliosEvent.ts
@@ -1,13 +1,22 @@
+/**
+ * @description This is the base class for all events in the system
+ */
 export class HeliosEvent {
-	constructor(private eventName: string) {
+	static readonly EVENT_TYPE: string = "defaultEventType";
+	readonly eventType: string;
+
+	constructor(private eventName: string, private eventMessage: string) {
 		this.eventName = eventName;
+		this.eventMessage = eventMessage;
+		this.eventType = HeliosEvent.EVENT_TYPE;
 	}
 
 	getEventName(): string {
 		throw new Error("getMessage() must be implemented");
 	}
 
-	getMessage(): string {
-		throw new Error("getName() must be implemented");
+	// method to serialize the event
+	serialize(): string {
+		return JSON.stringify(this);
 	}
 }
