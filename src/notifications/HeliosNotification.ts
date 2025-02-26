54738d75932dc92e4874df8219ce1681df29cdce
David Torres <davidj_torres@hotmail.com>
2025-02-26 13:45:00 -0500

    creating notification class

---
+++ src/notifications/HeliosNotification.ts
@@ -0,0 +1,31 @@
+/**
+ * @description This is the base class for all notifications in the system
+ */
+
+class HeliosNotification {
+	payload: string;
+	read: boolean;
+	userId: string;
+	deviceId: string;
+	email: string;
+	notificationType: string;
+	priority: 1 | 2 | 3;
+	createdAt: Date;
+	updatedAt: Date;
+
+	constructor(
+		payload: string,
+		userId: string,
+		notificationType: string,
+		priority: 1 | 2 | 3
+	) {
+		this.payload = payload;
+		this.read = false;
+		this.userId = userId;
+		this.notificationType = notificationType;
+		this.priority = priority;
+		this.createdAt = new Date();
+	}
+}
+
+export default HeliosNotification;
