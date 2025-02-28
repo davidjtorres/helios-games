8f3efa3506b8ce180ec62ae124dda7b53746ae46
David Torres <davidj_torres@hotmail.com>
2025-02-28 08:31:55 -0500

    adding NotificationDispatcherEnum and NotificationTypeEnum

--- src/common/enums.ts
+++ src/common/enums.ts
@@ -1,7 +1,19 @@
 export enum GameEventNameEnum {
-    PlayerLevelUp = "playerLevelUp",
-    PlayerAcquireItem = "playerAcquireItem",
-    PlayerCompleteQuest = "playerCompleteQuest",
-    PlayerCompleteAchievement = "playerCompleteAchievement",
-    PlayerVsPlayer = "playerVsPlayer",
+	PlayerLevelUp = "playerLevelUp",
+	PlayerAcquireItem = "playerAcquireItem",
+	PlayerCompleteQuest = "playerCompleteQuest",
+	PlayerCompleteAchievement = "playerCompleteAchievement",
+	PlayerVsPlayer = "playerVsPlayer",
+}
+
+export enum NotificationTypeEnum {
+	Email = "email",
+	Push = "push",
+	Web = "web",
+}
+
+export enum NotificationDispatcherEnum {
+	InApp = "in-app-notification-dispatcher",
+	Email = "email-notification-dispatcher",
+	Push = "push-notification-dispatcher",
 }
