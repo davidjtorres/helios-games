e07d2a16b370b92a8ca177685862630c0411d0a9
David Torres <davidj_torres@hotmail.com>
2025-02-28 14:41:27 -0500

    imporving typing

--- src/notifications/base-notification.ts
+++ src/notifications/base-notification.ts
@@ -2,21 +2,23 @@
  * @description This is the base class for all notifications in the system
  */
 
+import { EventTypeEnum, NotificationTypeEnum } from "../common/enums";
+
 class BaseNotification {
 	payload: string;
 	read: boolean;
 	userId: string;
-	deviceId: string;
-	email: string;
-	notificationType: string;
+	deviceId?: string;
+	email?: string;
+	notificationType: NotificationTypeEnum;
+	notificationSource: EventTypeEnum;
 	priority: 1 | 2 | 3;
 	createdAt: Date;
-	updatedAt: Date;
 
 	constructor(
 		payload: string,
 		userId: string,
-		notificationType: string,
+		notificationType: NotificationTypeEnum,
 		priority: 1 | 2 | 3
 	) {
 		this.payload = payload;
