29bc6e51741af79b0232e1a37d3c033bbc33d0d6
David Torres <davidj_torres@hotmail.com>
2025-02-28 14:19:24 -0500

    adding event name

--- src/events/game/player-acquire-item-event.ts
+++ src/events/game/player-acquire-item-event.ts
@@ -2,6 +2,7 @@ import { GameEvent } from "./base/game-event";
 import { GameEventNameEnum } from "../../common/enums";
 
 export class PlayerAcquireItemEvent extends GameEvent {
+	readonly eventName = GameEventNameEnum.PlayerAcquireItem;
 	constructor(
 		private readonly playerId: string,
 		private readonly itemId: string
--- src/events/game/player-level-up-event.ts
+++ src/events/game/player-level-up-event.ts
@@ -2,6 +2,8 @@ import { GameEvent } from "./base/game-event";
 import { GameEventNameEnum } from "../../common/enums";
 
 export class PlayerLevelUpEvent extends GameEvent {
+	readonly eventName = GameEventNameEnum.PlayerLevelUp;
+
 	constructor(
 		private readonly playerId: string,
 		private readonly levelReached: number
