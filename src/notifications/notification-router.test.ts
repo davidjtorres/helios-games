4aa8baa2dc46c6201b38dfa20e22a5b2fdd22767
David Torres <davidj_torres@hotmail.com>
2025-03-05 10:43:56 -0500

    addingm ore tests

--- src/notifications/notification-router.test.ts
+++ src/notifications/notification-router.test.ts
@@ -1,7 +1,11 @@
 import NotificationRouter from "./notification-router";
 import EventDispatcher from "../event-dispatcher/event-dispatcher";
 import StoreManager from "../services/store-manager";
-import { GameEventNameEnum, NotificationDispatcherEnum, NotificationTypeEnum } from "../common/enums";
+import {
+	GameEventNameEnum,
+	NotificationDispatcherEnum,
+	NotificationTypeEnum,
+} from "../common/enums";
 import BaseNotification from "./base-notification";
 import { GameEvent } from "../events/game/base/game-event";
 describe("NotificationRouter", () => {
@@ -56,9 +60,7 @@ describe("NotificationRouter", () => {
 		expect(notificationArg.userId).toBe("1");
 		expect(notificationArg.notificationType).toBe(NotificationTypeEnum.InApp);
 		expect(notificationArg.priority).toBe(1);
-		expect(notificationArg.payload).toBe(
-			"Player 1 reached level 2"
-		);
+		expect(notificationArg.payload).toBe("Player 1 reached level 2");
 	});
 
 	test("should create and dispatch a notification for playerAcquireItem game event", () => {
@@ -87,8 +89,64 @@ describe("NotificationRouter", () => {
 		expect(notificationArg.userId).toBe("1");
 		expect(notificationArg.notificationType).toBe(NotificationTypeEnum.InApp);
 		expect(notificationArg.priority).toBe(1);
-		expect(notificationArg.payload).toBe(
-			"Player 1 acquired item 1"
+		expect(notificationArg.payload).toBe("Player 1 acquired item 1");
+	});
+
+	test("should create and dispatch a notification for playerCompleteQuest game event", () => {
+		const event = new GameEvent(GameEventNameEnum.PlayerCompleteQuest, {
+			playerId: "1",
+			questId: "1",
+		});
+
+		mockEventDispatcher.dispatchEvent(event.eventType, event);
+
+		expect(mockStoreManager.getItem).toHaveBeenCalledWith(
+			"user_notification_preferences_1"
 		);
+
+		expect(mockEventDispatcher.dispatchEvent).toHaveBeenCalledWith(
+			NotificationDispatcherEnum.InApp,
+			expect.any(BaseNotification)
+		);
+
+		const dispatchEventMock = mockEventDispatcher.dispatchEvent as jest.Mock;
+		const notificationArg = dispatchEventMock.mock.calls.find(
+			(call) => call[0] === NotificationDispatcherEnum.InApp
+		)?.[1] as BaseNotification;
+
+		expect(notificationArg).toBeDefined();
+		expect(notificationArg.userId).toBe("1");
+		expect(notificationArg.notificationType).toBe(NotificationTypeEnum.InApp);
+		expect(notificationArg.priority).toBe(1);
+		expect(notificationArg.payload).toBe("Player 1 completed quest 1");
+	});
+
+	test("should create and dispatch a notification for playerCompleteAchievement game event", () => {
+		const event = new GameEvent(GameEventNameEnum.PlayerCompleteAchievement, {
+			playerId: "1",
+			achievementId: "1",
+		});
+
+		mockEventDispatcher.dispatchEvent(event.eventType, event);
+
+		expect(mockStoreManager.getItem).toHaveBeenCalledWith(
+			"user_notification_preferences_1"
+		);
+
+		expect(mockEventDispatcher.dispatchEvent).toHaveBeenCalledWith(
+			NotificationDispatcherEnum.InApp,
+			expect.any(BaseNotification)
+		);
+
+		const dispatchEventMock = mockEventDispatcher.dispatchEvent as jest.Mock;
+		const notificationArg = dispatchEventMock.mock.calls.find(
+			(call) => call[0] === NotificationDispatcherEnum.InApp
+		)?.[1] as BaseNotification;
+
+		expect(notificationArg).toBeDefined();
+		expect(notificationArg.userId).toBe("1");
+		expect(notificationArg.notificationType).toBe(NotificationTypeEnum.InApp);
+		expect(notificationArg.priority).toBe(1);
+		expect(notificationArg.payload).toBe("Player 1 achieved 1");
 	});
 });
