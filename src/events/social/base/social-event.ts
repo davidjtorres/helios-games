7e2cc1e28041cb875c3a20653a00bec54669b122
David Torres <davidj_torres@hotmail.com>
2025-03-06 09:45:28 -0500

    updating social event class properties

--- src/events/social/base/social-event.ts
+++ src/events/social/base/social-event.ts
@@ -1,9 +1,22 @@
 
 import { BaseEvent } from "../../base/base-event";
-import { EventTypeEnum } from "../../../common/enums";
-class SocialEvent extends BaseEvent {
-	static readonly EVENT_TYPE: string = EventTypeEnum.Social;
-	readonly eventType: string = EventTypeEnum.Social;
+import { EventTypeEnum, SocialEventNameEnum } from "../../../common/enums";
+
+interface SocialEventPayload {
+	playerId: string;
+	gameId: string;
+	targetPlayerId?: string;
+
+
 }
 
-export default SocialEvent;
+export class SocialEvent extends BaseEvent {
+	static readonly EVENT_TYPE: string = EventTypeEnum.Social;
+	readonly eventType: string = EventTypeEnum.Social;
+	readonly eventPayload: SocialEventPayload;
+
+	constructor(eventName: SocialEventNameEnum, eventPayload: SocialEventPayload) {
+		super(SocialEvent.EVENT_TYPE, eventName);
+		this.eventPayload = eventPayload;
+	}
+}
