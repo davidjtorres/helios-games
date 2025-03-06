7e705dec9da368f422f45d76306ba70e7b3e0d85
David Torres <davidj_torres@hotmail.com>
2025-03-06 15:55:36 -0500

    simulation calls

--- index.ts
+++ index.ts
@@ -1,8 +1,27 @@
-
+import GameEngine from "./src/engines/game-engine";
+import SocialEngine from "./src/engines/social-engine";
+import EventDispatcher from "./src/event-dispatcher/event-dispatcher";
+import InAppNotificationDispatcher from "./src/notifications/in-app-notification-dispatcher";
+import NotificationRouter from "./src/notifications/notification-router";
+import StoreManager from "./src/services/store-manager";
 
 function main() {
-	console.log("Hello World");
+	const eventDispatcher = new EventDispatcher();
+	const gameEngine = new GameEngine(eventDispatcher);
+	const socialEngine = new SocialEngine(eventDispatcher);
+	const storeManager = new StoreManager();
+	const notificationRouter = new NotificationRouter(
+		eventDispatcher,
+		storeManager
+	);
+	const inAppNotificationDispatcher = new InAppNotificationDispatcher(
+		eventDispatcher
+	);
+	gameEngine.playerAcquireItem("1", "1");
+	gameEngine.playerLevelUp("1", 1);
+	socialEngine.sendFriendRequest("1", "2", "1");
+	socialEngine.acceptFriendRequest("2", "1", "1");
+	socialEngine.followUser("1", "2", "1");
 }
-
 
 main();
