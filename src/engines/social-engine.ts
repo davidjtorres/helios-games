91c716ad168cdc837f2008e6dce73fec4471e6e8
David Torres <davidj_torres@hotmail.com>
2025-03-06 14:16:14 -0500

    implementing simulation methods

--- src/engines/social-engine.ts
+++ src/engines/social-engine.ts
@@ -1,28 +1,46 @@
 import EventDispatcher from "../event-dispatcher/event-dispatcher";
-import { FriendRequest } from "../events/social/friend-request";
-import { FriendRequestAccepted } from "../events/social/friend-request-accepted";
+import { SocialEvent } from "../events/social/base/social-event";
+import { SocialEventNameEnum } from "../common/enums";
 
 class SocialEngine {
 	constructor(private eventDispatcher: EventDispatcher) {
 		this.eventDispatcher = eventDispatcher;
 	}
 
-	sendFriendRequest(requesterId: string, targetId: string) {
-		const event = new FriendRequest(
-			this.eventDispatcher,
-			requesterId,
-			targetId
+	sendFriendRequest(requesterId: string, targetId: string, gameId: string) {
+		const event = new SocialEvent(
+			SocialEventNameEnum.FriendRequest,
+			{
+				playerId: requesterId,
+				targetPlayerId: targetId,
+				gameId: gameId
+			}
 		);
-		this.eventDispatcher.dispatchEvent(event.eventName, event.toJSON());
+		this.eventDispatcher.dispatchEvent(event.eventType, event);
 	}
 
-	acceptFriendRequest(accepterId: string, requesterId: string) {
-		const event = new FriendRequestAccepted(
-			this.eventDispatcher,
-			accepterId,
-			requesterId
+	acceptFriendRequest(accepterId: string, requesterId: string, gameId: string) {
+		const event = new SocialEvent(
+			SocialEventNameEnum.FriendRequestAccepted,
+			{
+				playerId: accepterId,
+				targetPlayerId: requesterId,
+				gameId: gameId
+			}
 		);
-		this.eventDispatcher.dispatchEvent(event.eventName, event.toJSON());
+		this.eventDispatcher.dispatchEvent(event.eventType, event);
+	}
+
+	followUser(followerId: string, followingId: string, gameId: string) {
+		const event = new SocialEvent(
+			SocialEventNameEnum.FollowUser,
+			{
+				playerId: followerId,
+				targetPlayerId: followingId,
+				gameId: gameId
+			}
+		);
+		this.eventDispatcher.dispatchEvent(event.eventType, event);
 	}
 }
 
